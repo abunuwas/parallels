4fffe6c08fc779e400edba5cdf3e9525f7db352f
abunuwas <joseharoperalta@gmail.com>
2016-03-31 17:45:12 +0200

    Make functions to run producer either with multiprocessing or with multithreading

--- producer.py
+++ producer.py
@@ -7,8 +7,9 @@ import os
 import threading 
 
 
+
 def generate_random_int(ID):
-	print(randint(1,1000), ID)
+	return randint(1,1000)
 
 def events(num_events, ID):
 	scheduler = sched.scheduler(time.time, time.sleep)
@@ -16,7 +17,7 @@ def events(num_events, ID):
 		scheduler.enter(randint(0,3), randint(0,3), generate_random_int, (ID,))
 	scheduler.run()
 
-if __name__ == '__main__':
+def event_threading():
 	jobs = []
 	for i in range(0,10):
 		process = threading.Thread(target=events, args=(10,i))
@@ -27,3 +28,18 @@ if __name__ == '__main__':
 
 	for j in jobs:
 		j.join()
+
+def event_processing():
+	jobs = []
+	for i in range(0,10):
+		process = mp.Process(target=events, args=(10,i))
+		jobs.append(process)
+
+	for j in jobs:
+		j.start()
+
+	for j in jobs:
+		j.join()
+
+if __name__ == '__main__':
+	event_threading()
