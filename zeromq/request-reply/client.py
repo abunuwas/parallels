0bd14bcfb69b769f98835fefedb473eb3513aa56
=abunuwas <=>
2016-04-13 17:52:38 +0100

    Improve message capturing from the comand line

--- zeromq/request-reply/client.py
+++ zeromq/request-reply/client.py
@@ -9,5 +9,7 @@ sock = context.socket(zmq.REQ)
 sock.connect("tcp://127.0.0.1:5678")
 
 # Send a "message" using the socket
-sock.send_string(" ".join(sys.argv[1:]))
+if len(sys.argv) > 1:
+	message = sys.argv[1:]
+	sock.send_string(" ".join(message))
 print(sock.recv())
