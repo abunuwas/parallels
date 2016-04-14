1b6282483d5d9b86e673114fd8311385f18e4045
abunuwas <joseharoperalta@gmail.com>
2016-04-14 09:20:39 +0100

    Start video-producer project for testing purposes

---
+++ video-producer/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from flask import Flask, Response, render_template
+from camera import camera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--framez\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+	return Response(gen(Camera()),
+					mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
+
+
